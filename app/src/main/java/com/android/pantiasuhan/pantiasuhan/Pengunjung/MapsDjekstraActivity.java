49c62841fcf66a59f33359ca4891157f0c60282f
wahyuekojustino <wahyuekojustino@gmail.com>
2020-08-08 14:22:04 +0700

    java djektra

--- app/src/main/java/com/android/pantiasuhan/pantiasuhan/Pengunjung/MapsDjekstraActivity.java
+++ app/src/main/java/com/android/pantiasuhan/pantiasuhan/Pengunjung/MapsDjekstraActivity.java
@@ -52,7 +52,7 @@ public class MapsDjekstraActivity extends FragmentActivity implements OnMapReady
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.activity_maps_djekstra);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
